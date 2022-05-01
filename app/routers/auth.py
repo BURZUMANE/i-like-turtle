0c62515723234d528a1de5fff97cfc7d284fd54a
timZ <timur.zakirov@zesty.co>
2022-05-01 11:10:58 +0300

    auth route response status fix

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -15,10 +15,10 @@ async def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Ses
     user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
 
     if not user:
-        raise HTTPException(status_code=status.HTTP_403_NOT_FOUND, detail=f'invalid credentials')
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'invalid credentials')
 
     if not verify_password(user_credentials.password, user.password):
-        raise HTTPException(status_code=status.HTTP_403_NOT_FOUND, detail=f'invalid credentials')
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'invalid credentials')
 
     access_token = create_access_token(data={"user_id": user.id})
     return {"access_token": access_token, "token_type": "bearer"}
