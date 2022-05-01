5be14c6132df623bfec012b4ab579667f654ab79
timZ <timur.zakirov@zesty.co>
2022-05-01 11:06:41 +0300

    fix create user duplicate error

--- app/routers/users.py
+++ app/routers/users.py
@@ -14,6 +14,12 @@ router = APIRouter(
 
 @router.post('', status_code=status.HTTP_201_CREATED, response_model=schemas.UserResponse)
 async def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    print(user.email)
+    existing_user = db.query(models.User).filter(models.User.email == user.email).first()
+    print(existing_user)
+    if existing_user:
+        raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=f'user with the given email already exists')
+
     hashed_password = get_password_hash(user.dict()['password'])
     user.password = hashed_password
     new_user = models.User(**user.dict())
