2cbf78f43578adc3e75a0864560aa46e6b01bd76
timZ <timur.zakirov@zesty.co>
2022-05-05 13:50:19 +0300

    commit

--- app/database.py
+++ app/database.py
@@ -11,7 +11,7 @@ from sqlalchemy.orm import sessionmaker
 from app.config import settings
 
 SQLALCHEMY_DATABASE_URL =\
-    f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}/{settings.database_name}'
+    f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 
 print(SQLALCHEMY_DATABASE_URL)
 
